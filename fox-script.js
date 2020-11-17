fd3dad88bc570ff9dc8b3d616beeed31d20f1488
carloso3 <74416127+carloso3@users.noreply.github.com>
2020-11-16 22:04:50 -0600

    Update fox-script.js

--- fox-script.js
+++ fox-script.js
@@ -1,3 +1,7 @@
-console.log("fox");
-document.body.style.backgroundColor = "orange";
+console.log("cnn");
 let url = location.href;
+if (url === "https://www.foxnews.com/politics") {
+    var x = document.getElementsByClassName("");
+    // choose random array value to flag
+    x[Math.floor(Math.random() * x.length)].style.backgroundColor = "red";
+}
